3f7b72a84c85a7e111b48dfb8dbc57da4f15a7bf
Razmik Ayvazyan <79054971+ayvazyan10@users.noreply.github.com>
2024-07-16 15:03:07 +0400

    Update converter.py
    added recurs.

--- converter.py
+++ converter.py
@@ -1,19 +1,19 @@
 import os
 from PIL import Image
 
-
 def convert_and_resize_images(folder_path):
-    for filename in os.listdir(folder_path):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
-            img_path = os.path.join(folder_path, filename)
-            with Image.open(img_path) as img:
-                img = img.resize((1980, int(1980 * img.height / img.width)), Image.Resampling.LANCZOS)
+    for root, _, files in os.walk(folder_path):
+        for filename in files:
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+                img_path = os.path.join(root, filename)
+                with Image.open(img_path) as img:
+                    img = img.resize((1980, int(1980 * img.height / img.width)), Image.Resampling.LANCZOS)
 
-                new_filename = os.path.splitext(filename)[0] + '.webp'
-                new_img_path = os.path.join(folder_path, new_filename)
-                img.save(new_img_path, 'webp')
+                    new_filename = os.path.splitext(filename)[0] + '.webp'
+                    new_img_path = os.path.join(root, new_filename)
+                    img.save(new_img_path, 'webp')
 
-                print(f'Converted and resized {filename} to {new_filename}')
+                    print(f'Converted and resized {filename} to {new_filename}')
 
 
 if __name__ == "__main__":
