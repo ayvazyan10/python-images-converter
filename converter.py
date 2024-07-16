ca6fd30cd9dda012d2553c109e11878b95863fd6
Razmik Ayvazyan <79054971+ayvazyan10@users.noreply.github.com>
2024-07-16 13:21:05 +0400

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,24 @@
+import os
+from PIL import Image
+
+
+def convert_and_resize_images(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
+            img_path = os.path.join(folder_path, filename)
+            with Image.open(img_path) as img:
+                img = img.resize((1980, int(1980 * img.height / img.width)), Image.Resampling.LANCZOS)
+
+                new_filename = os.path.splitext(filename)[0] + '.webp'
+                new_img_path = os.path.join(folder_path, new_filename)
+                img.save(new_img_path, 'webp')
+
+                print(f'Converted and resized {filename} to {new_filename}')
+
+
+if __name__ == "__main__":
+    folder_path = input("Enter the folder path: ").strip()
+    if os.path.isdir(folder_path):
+        convert_and_resize_images(folder_path)
+    else:
+        print("The provided path is not a valid directory.")
