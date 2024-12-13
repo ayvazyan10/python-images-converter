7f5ac24794ebbdf6d4b1e4c9e515769f49edc0b7
Razmik Ayvazyan <79054971+ayvazyan10@users.noreply.github.com>
2024-12-13 18:31:51 +0400

    resize only image that greater then 1920px

--- converter.py
+++ converter.py
@@ -1,19 +1,26 @@
 import os
 from PIL import Image
 
+
 def convert_and_resize_images(folder_path):
     for root, _, files in os.walk(folder_path):
         for filename in files:
             if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.tiff')):
                 img_path = os.path.join(root, filename)
                 with Image.open(img_path) as img:
-                    img = img.resize((1980, int(1980 * img.height / img.width)), Image.Resampling.LANCZOS)
+                    if img.width > 1920:
+                        new_width = 1920
+                        new_height = int(new_width * img.height / img.width)
+                        img = img.resize((new_width, new_height), Image.Resampling.LANCZOS)
+                        print(f'Resized {filename} to 1920 width')
+                    else:
+                        print(f'Converted {filename} without resizing (width <= 1920)')
 
                     new_filename = os.path.splitext(filename)[0] + '.webp'
                     new_img_path = os.path.join(root, new_filename)
                     img.save(new_img_path, 'webp')
 
-                    print(f'Converted and resized {filename} to {new_filename}')
+                    print(f'Saved {filename} as {new_filename}')
 
 
 if __name__ == "__main__":
